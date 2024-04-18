05db6940bd41de7d1ae4b1f41e20d94512abdb7b
joaocerqueira <joaokruschewsky@gmail.com>
2024-04-18 20:47:06 -0300

    data fetching

---
+++ components/project-list.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>Projects</div>
+</template>
+
+<script setup>
+const callApi = await useFetch('https://api.github.com/users/piotr-jura-udemy/repos');
+console.log(callApi);
+</script>
